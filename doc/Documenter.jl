7a1ae109583a9e66913f68b512671c93a6f0e7be
Aero-Spec <Anshrin@gmail.com>
2024-08-31 14:03:22 -0400

    Update Documenter.jl

--- doc/Documenter.jl
+++ doc/Documenter.jl
@@ -1,1 +1,20 @@
+name: Documentation
+
+on:
+  push:
+    branches:
+      - main  # or master or your main branch
+    tags: '*'
+  pull_request:
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - uses: julia-actions/setup-julia@v1
+        with:
+          version: '1.6'
+      - run: julia --project=docs/ -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.instantiate()'
+      - run: julia --project=docs/ docs/make.jl
 
