7567b3b819d67487a7d8cd8caacdc3243d2d72a5
Aero-Spec <Anshrin@gmail.com>
2024-08-31 14:00:37 -0400

    Update make.jl

--- doc/make.jl
+++ doc/make.jl
@@ -1,17 +1,17 @@
-using Documenter
-using DividedRectangles
+using Documenter, DividedRectangles
 
 makedocs(
-    sitename = "DividedRectangles.jl",
+    sitename = "DividedRectangles.jl Documentation",
     modules = [DividedRectangles],
-    format = Documenter.HTML(prettyurls = true),
+    format = Documenter.HTML(prettyurls=true),
     pages = [
         "Home" => "index.md",
+        "Installation" => "installation.md",
+        "Usage" => "usage.md",
+        "Theory" => "theory.md",
+        "Advanced Usage" => "advanced_usage.md",
+        "Visualization" => "visualization.md",
+        "Credits" => "credits.md"
     ],
-    deploy_config = Documenter.DeployConfig(
-        target = "gh-pages",
-        repo = "aero-spec/DividedRectangles.jl",
-        devbranch = "main",
-        folder = "docs",
-    )
+    assets = "assets",
 )
